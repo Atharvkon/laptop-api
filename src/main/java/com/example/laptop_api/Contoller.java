7e31e681a182e9266f93ddecbac30bf66afd90d9
Atharv <atharvkondle897@gmail.com>
2026-06-17 22:11:46 +0530

    new checkstyle

--- src/main/java/com/example/laptop_api/Contoller.java
+++ src/main/java/com/example/laptop_api/Contoller.java
@@ -2,11 +2,17 @@ package com.example.laptop_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
-public class Contoller {
+public class Contoller
+{
   @GetMapping("/greet")
   public String greet(String name) {
-    return "Hello " + name;
+    String Greeting = "Hello " + name;
+    return Greeting;
+  }
+
+  public void unused_method(){
   }
 }
