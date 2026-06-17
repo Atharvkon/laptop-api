9616b799fd56b4adbcf822496829dc37215efc8a
Atharv <atharvkondle897@gmail.com>
2026-06-17 20:21:53 +0530

    ndjcnejc

--- src/main/java/com/example/laptop_api/Contoller.java
+++ src/main/java/com/example/laptop_api/Contoller.java
@@ -1,14 +1,9 @@
 package com.example.laptop_api;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Contoller {
-
-    @GetMapping("/greet")
-    public String greet(String name) {
-        return "Hello " + name;
+public class Contoller{ @GetMapping("/greet")
+    public String greet(String name){return "Hello "+name;
     }
-
 }
