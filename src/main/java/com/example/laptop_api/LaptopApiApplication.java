95791aafa6ab15e92f19927f1f3a2f19f7a688b4
Atharv <atharvkondle897@gmail.com>
2026-06-17 20:34:28 +0530

    formatted the code

--- src/main/java/com/example/laptop_api/LaptopApiApplication.java
+++ src/main/java/com/example/laptop_api/LaptopApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LaptopApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LaptopApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LaptopApiApplication.class, args);
+  }
 }
