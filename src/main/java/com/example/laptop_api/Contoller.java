bf9974bd1a8bc0388f0ee8691d79fbe615c7974a
Atharv <atharvkondle897@gmail.com>
2026-06-17 22:22:54 +0530

    dejdnejdej

--- src/main/java/com/example/laptop_api/Contoller.java
+++ src/main/java/com/example/laptop_api/Contoller.java
@@ -2,17 +2,20 @@ package com.example.laptop_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.*;
 
+/** REST controller that handles greeting-related endpoints. */
 @RestController
-public class Contoller
-{
+public class Contoller {
+
+  /**
+   * Returns a greeting message for the given name.
+   *
+   * @param name the name of the person to greet
+   * @return a greeting string containing the provided name
+   */
   @GetMapping("/greet")
   public String greet(String name) {
-    String Greeting = "Hello " + name;
-    return Greeting;
-  }
-
-  public void unused_method(){
+    String greeting = "Hello " + name;
+    return greeting;
   }
 }
