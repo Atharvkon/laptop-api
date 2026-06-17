eec8336fd410258bda4f98c14b656986b167758f
Atharv <atharvkondle897@gmail.com>
2026-06-17 19:45:11 +0530

    cdnjcndjc

---
+++ src/main/java/com/example/laptop_api/Contoller.java
@@ -0,0 +1,14 @@
+package com.example.laptop_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Contoller {
+
+    @GetMapping("/greet")
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+
+}
---
+++ src/main/java/com/example/laptop_api/LaptopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.laptop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopApiApplication.class, args);
+	}
+
+}
